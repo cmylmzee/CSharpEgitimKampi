bc5d181ca5d1b582c14198ea54708e5fe0e814cf
Cem Yılmaz <yilmazcem1122@gmail.com>
2024-10-08 15:21:13 +0300

    Ders 4 ve 5 örnekleri yapıldı

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Loops
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("c# egitim kampi");
+            //}
+
+
+            //for (int i = 3; i <= 50; i+=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.WriteLine("Please enter the loop number");
+            //int finalNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the text");
+            //string text = Console.ReadLine();
+
+            //for (int i = 0; i < finalNumber; i++)
+            //{
+            //    Console.WriteLine(text);
+            //}
+
+
+            #endregion
+
+
+            #region While Loop
+
+            //int i = 5;
+
+            //while (i<10) {
+            //    i++;
+            //    Console.WriteLine(i);
+            //}
+
+            #endregion
+
+            // SORU : KLAVYEDEN GİRİLEN 3 BASAMAKLI SAYININ BASAMAKLARI TOPLAMINI HESAPLA
+
+            //Console.WriteLine("Please enter the number");
+            //int i  = int.Parse(Console.ReadLine());
+
+            //int first,second,third;
+
+            //first = i / 100;
+            //second = (i % 100) / 10 ;
+            //third = i % 10;
+
+            //Console.WriteLine(first + " "  + second + " " + third); 
+
+            Console.Read();
+        }
+    }
+}
---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region How To Create Star With Loops ? 
+
+            // DİK ÜÇGEN FORMUNDA OLUŞTURMA
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // Ters Dik Üçgen
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+            #region Dik ve Ters Dik üçgen beraber
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Dilimi
+
+            //  Baklavanın Üstü
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //// Baklavanın Altı
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Piramit
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
